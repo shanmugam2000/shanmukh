7522679b53ccf25e7d954c31470d86314e27e3f1
shanmugam2000 <100746941+shanmugam2000@users.noreply.github.com>
2022-03-03 11:44:09 +0530

    odd

---
+++ condition.py
@@ -0,0 +1,7 @@
+ a = 40
+
+if a > 20:
+    print("is greater then 20")
+
+else:
+    print("is lesser then 20")
---
+++ evenodd.py
@@ -0,0 +1,10 @@
+a = -4
+
+if a == 0:
+    print("it's a 0")
+
+elif a > 0:
+    print("it's > than 0")
+
+else:
+    print("its < than 0")
