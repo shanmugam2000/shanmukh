43963fbe8fa7c0ab4497b6c3966765ff6408fa77
shanmugam2000 <100746941+shanmugam2000@users.noreply.github.com>
2022-03-08 15:18:06 +0530

    findingroll

--- retrievestudent.py
+++ retrievestudent.py
@@ -1,10 +1,10 @@
 import sqlite3
 connection = sqlite3.connect("college.db")
-
-result = connection.execute("select * from student")
+getroll = input("enter the rollnumber to be searched ? ")
+result = connection.execute("select * from student where rollnumber="+getroll)
 
 for i in result:
-    print("name",i[1])
-    print("rollnumber",i[2])
-    print("admno ",i[3])
-    print("college ",i[4])
+    print("name =>",i[1])
+    print("rollnumber =>",i[2])
+    print("admno =>",i[3])
+    print("college =>",i[4])
