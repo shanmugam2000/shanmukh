804fa1097e19bef7967a6adc90db3f5766763195
shanmugam2000 <100746941+shanmugam2000@users.noreply.github.com>
2022-03-08 12:16:51 +0530

    data base table

--- student.py
+++ student.py
@@ -2,16 +2,23 @@ import sqlite3
 
 connection = sqlite3.connect("college.db")
 
-connection.execute(''' create table student(
-                       id integer primary key autoincrement,
-                       name text,
-                       rollnumber integer,
-                       admno integer,
-                       college text
+#connection.execute(''' create table student(
+#                    id integer primary key autoincrement,
+#                     name text,
+#                     rollnumber integer,
+#                     admno integer,
+#                     college text
                        
                
-
-
-);   ''')
+#);   ''')
 
 print("table created successfully")
+getName = input("Enter Name : ")
+getRollNumber = input("Enter RollNumber : ")
+getAdmno = input("Enter Admno : ")
+getCollege = input("Enter College name : ")
+connection.execute("INSERT INTO student(name,rollnumber,admno,college) \
+    VALUES('"+getName+"',"+getRollNumber+","+getAdmno+",'"+getCollege+"')")
+connection.commit()
+connection.close()
+print("Inserted succesfully")
